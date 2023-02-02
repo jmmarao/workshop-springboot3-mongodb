2be52e92f9c8016e175ec9ecea30ebbb147981ef
Jones <jmmarao@gmail.com>
2023-02-01 22:44:04 -0300

    Connect MongoDB with repository. UserService implemented

--- src/main/java/com/jmmarao/workshopspringboot3mongodb/controllers/UserController.java
+++ src/main/java/com/jmmarao/workshopspringboot3mongodb/controllers/UserController.java
@@ -1,24 +1,23 @@
 package com.jmmarao.workshopspringboot3mongodb.controllers;
 
 import com.jmmarao.workshopspringboot3mongodb.entities.User;
+import com.jmmarao.workshopspringboot3mongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public ResponseEntity<List<User>> findUsersController() {
-        List<User> users = new ArrayList<>();
-        User user1 = new User(0L, "Doctor Strange", "strange@email.com");
-        User user2 = new User(1L, "Hulk", "smash@email.com");
-        users.addAll(Arrays.asList(user1, user2));
-        return ResponseEntity.ok().body(users);
+        return ResponseEntity.ok().body(userService.findUsers());
     }
 }
---
+++ src/main/java/com/jmmarao/workshopspringboot3mongodb/services/UserService.java
@@ -0,0 +1,18 @@
+package com.jmmarao.workshopspringboot3mongodb.services;
+
+import com.jmmarao.workshopspringboot3mongodb.entities.User;
+import com.jmmarao.workshopspringboot3mongodb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+}
